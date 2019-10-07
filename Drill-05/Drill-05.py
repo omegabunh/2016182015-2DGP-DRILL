f352c05e1a59f3430cbb1e8995a75ae9821ee3d1
Seongwoo Park <omegabun@gmail.com>
2019-10-07 19:20:38 +0900

    complete
    2016182015 박성우 Drill-05 완료
    커밋횟수: 3회

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -2,50 +2,67 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-
 def handle_events():
     global running
     global x, y
-    global _x, _y
+    global mouseX, mouseY
     global side
+
     events = get_events()
+
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.key == SDL_MOUSEMOTION:
-            _x, _y = event.x + 20, KPU_HEIGHT - 1 - event.y - 30
+
+        elif event.type == SDL_MOUSEMOTION:
+            mouseX, mouseY = event.x + 20, KPU_HEIGHT - 1 - event.y - 30
         elif event.button == SDL_BUTTON_LEFT:
             if x < event.x:
                 x, y = event.x, KPU_HEIGHT - 1 - event.y
                 side = 1
-            elif x > event.x:
+            elif event.x < x:
                 x, y = event.x, KPU_HEIGHT - 1 - event.y
                 side = 0
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-    pass
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+mouse_point = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x1, y1 = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mouseX, mouseY = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
-show_cursor()
-side = 0
+side = 1
+hide_cursor()
+
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * side, 100, 100, x, y)
+    mouse_point.draw_now(mouseX, mouseY)
+    character.clip_draw(frame * 100, 100 * side, 100, 100, x1, y1)
     update_canvas()
-    frame = (frame + 1) % 8
-    delay(0.05)
     handle_events()
+    frame = (frame + 1) % 8
+
+    if x1 < x and y1 < y:
+        x1 += (x - x1) / 3
+        y1 += (y - y1) / 3
+    elif x1 < x and y < y1:
+        x1 += (x - x1) / 3
+        y1 -= (y1 - y) / 3
+    elif x < x1 and y1 < y:
+        x1 -= (x1 - x) / 3
+        y1 += (y - y1) / 3
+    elif x < x1 and y < y1:
+        x1 -= (x1 - x) / 3
+        y1 -= (y1 - y) / 3
+
+
+    delay(0.05)
 
 close_canvas()
-
-
-
-
