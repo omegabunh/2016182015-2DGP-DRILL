c09d5d1a6dd32cc573ea8bc9afc4ae412d90a160
Seongwoo Park <omegabun@gmail.com>
2019-10-09 18:36:30 +0900

    Drill-06-02
    ing3...

--- Drill-06/Drill-06-02/Drill-06-02.py
+++ Drill-06/Drill-06-02/Drill-06-02.py
@@ -4,7 +4,7 @@ import random
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
 
-def move(p1, p2, p3, p4):
+def move(p1, p2, p3):
     global x, y, side
     global frame
     if p1[0] < p2[0]:
@@ -14,13 +14,13 @@ def move(p1, p2, p3, p4):
 
     for i in range(0, 100, 2):
         t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p4[0] + (3*t**3 - 5*t**2 + 2)*p1[0] + (-3*t**3 + 4*t**2 + t)*p2[0] + (t**3 - t**2)*p3[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p4[1] + (3*t**3 - 5*t**2 + 2)*p1[1] + (-3*t**3 + 4*t**2 + t)*p2[1] + (t**3 - t**2)*p3[1])/2
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
         character.clip_draw(frame * 100, 100 * side, 100, 100, x, y)
         for i in range(0, 10):
-            character.clip_draw(frame * 100, 100 * side, 100, 100, p2[0], p2[1])
+            character.clip_draw(frame * 100, 100 * side, 100, 100, p3[0], p3[1])
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.05)
@@ -35,8 +35,10 @@ points = [(random.randint(0 + 100, KPU_WIDTH-100), random.randint(0+100, KPU_HEI
 n = 1
 
 while True:
-    p1, p2, p3, p4 = points[n-3], points[n-2], points[n-1], points[n]
+    #p1, p2, p3, p4 = points[n-3], points[n-2], points[n-1], points[n]
+    p1, p2, p3 = points[n - 2], points[n - 1], points[n]
     n = (n + 1) % size
-    move(p1, p2, p3, p4)
+    move(p1, p2, p3)
+    #move(p1, p2, p3, p4)
 
 close_canvas()
